00fb38d03ad9360a476b876a795a18102d030e34
hitesh <hiteshkukaramprajapati@gmail.com>
2025-04-22 10:51:48 +0530

    Delete Product

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('product-list/',views.product_list,name='product_list'),
     path('product/create/',views.product_create,name='product_create'),
-    path('product-detail/<int:id>',views.product_detail,name='product_detail'),
+    path('product-detail/<int:id>/',views.product_detail,name='product_detail'),
+    path('product-delete/<int:id>/',views.product_delete,name='product_delete'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Product
 def product_list(request):
     products = Product.objects.all()
@@ -8,5 +8,13 @@ def product_create(request):
     return render(request,'inventory/create_product.html' )
 
 def product_detail(request,id):
-    product = Product.objects.get(id=id)
+    product = get_object_or_404(Product,id=id)
     return render(request,'inventory/product_detail.html',{'product':product})
+
+def product_delete(request,id):
+    product = get_object_or_404(Product,id=id)
+    if request.method == "GET":
+        return render(request,'inventory/delete_confirmation.html',{'product':product})
+    elif request.method == "POST":
+        product.delete()
+    return redirect('product_list')
