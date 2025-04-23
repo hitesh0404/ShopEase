85e54f170366bc37b8c9a323acd815dced986197
hitesh <hiteshkukaramprajapati@gmail.com>
2025-04-23 11:08:57 +0530

    forms

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('product-list/',views.product_list,name='product_list'),
-    path('product/create/',views.product_create,name='product_create'),
+    path('product/create/',views.ProductCreate.as_view(),name='product_create'),
     path('product-detail/<int:id>/',views.product_detail,name='product_detail'),
     path('product-delete/<int:id>/',views.product_delete,name='product_delete'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -4,8 +4,21 @@ def product_list(request):
     products = Product.objects.all()
     return render(request, 'inventory/product_list.html', {'products': products})
 
-def product_create(request):
-    return render(request,'inventory/create_product.html' )
+from django.views import View
+from .models import Category,Brand
+from .forms import ProductCreateForm
+class ProductCreate(View):
+    def get(self,request): 
+        # categories = Category.objects.all()
+        # brands =  Brand.objects.all()
+        # context = {
+        #     'categories' : categories,
+        #     'brands' : brands,
+        # }
+        form = ProductCreateForm()
+        return render(request,'inventory/create_product.html',{'form':form} )
+    def post(self,request):
+        pass
 
 def product_detail(request,id):
     product = get_object_or_404(Product,id=id)
