d7b5b825a7ef6c28bd77bebb48d82abe675da362
hitesh <hiteshkukaramprajapati@yahoo.com>
2025-05-22 10:04:35 +0530

    Mysql Db

--- ShopEase/settings.py
+++ ShopEase/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'ShopEase.wsgi.application'
 import os 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         # 'NAME':BASE_DIR / 'db.sqlite3',
         'OPTIONS':{
         'read_default_file': os.path.join(BASE_DIR,'my.cnf'),
